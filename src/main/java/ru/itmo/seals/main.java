d24a32a6a4e18ad62e46876735d61cb132f40ede
Mark Mozzhukhin <164361067+fireflys07@users.noreply.github.com>
2026-02-27 17:47:10 +0300

    Just test how it work ^^

--- src/main/java/ru/itmo/seals/main.java
+++ src/main/java/ru/itmo/seals/main.java
@@ -2,6 +2,9 @@ package ru.itmo.seals;
 
 public class main {
     public static void main(String[] args) {
+        while (true){
+            System.out.println("Проверка связи 1,2,3..");
+        }
 
     }
 }
